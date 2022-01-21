3ee5aae51cdff4f7ee92982be0025aef72dfb747
j0nimost <john.nyingi@live.com>
2022-01-21 13:02:58 +0300

    Fix from data to actions.payload

--- src/state/reducers/forecastReducer.js
+++ src/state/reducers/forecastReducer.js
@@ -1,7 +1,7 @@
 const forecastReducer = (state=[], actions) => {
     switch (actions.type) {
         case 'addForecast':
-            let dateObj = new Date(data.location.localtime);
+            let dateObj = new Date(actions.payload.location.localtime);
             const hour = parseInt(dateObj.getHours());
             return [...actions.payload.hour.slice(hour, hour+6)];
     
